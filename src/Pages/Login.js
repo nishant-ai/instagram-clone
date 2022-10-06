8e32d9a1201f46e06a2213ab1f05780e45e028a4
Ayush Pandit <ayushpanditmoto@gmail.com>
2022-10-06 21:07:25 +0530

    added login and register pages but needed css improvemet

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,63 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Login() {
-  return <div>Login</div>
+  return (
+    //instagram login page
+    <LoginContainer>
+      <LoginContain>
+        <img
+          src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
+          alt=''
+        />
+        <input type='text' placeholder='Username' />
+        <input type='password' placeholder='Password' />
+        <button>Log In</button>
+      </LoginContain>
+    </LoginContainer>
+  )
 }
 
 export default Login
+
+const LoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #fafafa;
+`
+
+const LoginContain = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  border: 1px solid lightgray;
+  border-radius: 1px;
+  width: 350px;
+  height: 400px;
+  img {
+    width: 175px;
+    height: 51px;
+    margin: 20px 0;
+  }
+  input {
+    width: 250px;
+    height: 35px;
+    border: 1px solid lightgray;
+    border-radius: 3px;
+    margin: 5px 0;
+    padding: 0 10px;
+  }
+  button {
+    width: 250px;
+    height: 30px;
+    border: none;
+    border-radius: 3px;
+    background-color: #0095f6;
+    color: white;
+    font-weight: 600;
+    margin: 10px 0;
+  }
+`
